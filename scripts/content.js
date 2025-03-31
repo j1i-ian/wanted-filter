84370e37a01baebeaf5fc1233fc4330671ce7775
Alan Lee <pdh2ndtt@duck.com>
2025-03-31 14:08:39 +0900

    Implemented content script init: wait for loading, extracting company id, name

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,34 @@
-
 (async () => {
+    await waitForLoading();
 
+    const jobPostersContainer = document.querySelector('[data-cy="job-list"]')
+    const jobPosterList = Array.from(jobPostersContainer.children);
+
+    jobPosterList.forEach(jobPoster => {
+        const jobCard = jobPoster.querySelector('[data-cy="job-card"]');
+        const cardAnchor = jobCard.querySelector('a');
+
+        const companyId = +cardAnchor.dataset['companyId'];
+        const companyName = cardAnchor.dataset['companyName'];
+
+        console.log(companyId);
+        console.log(companyName);
+    })
 })();
+
+function waitForLoading() {
+
+    return new Promise(resolve => {
+        const jobPostersContainer = document.querySelector('[data-cy="job-list"]')
+        const jobPostersContainerObserver = new MutationObserver((mutations, observer) => {
+            resolve(mutations);
+        });
+
+        jobPostersContainerObserver.observe(jobPostersContainer, {
+            childList: true,
+            subtree: true,
+            attributes: false,
+            characterData: false
+        });
+    })
+}
