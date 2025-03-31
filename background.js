3c779e765eaedad542a64017eeb90bf5151615c5
Alan Lee <pdh2ndtt@duck.com>
2025-03-31 14:38:07 +0900

    Implemented message event listener for checking blacklisted company

--- background.js
+++ background.js
@@ -2,3 +2,28 @@
 const APP_DATABASE_NAME = 'wantedFilterDatabase'
 const BLACKLIST_COMPANY_STORE_KEY = 'blacklistCompanyStore'
 
+const Action = {
+    CHECK_BLOCKLISTED: 'CHECK_BLOCKLISTED'
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    (async () => {
+
+        const { companyId } = request;
+
+        switch (request.action) {
+            case Action.CHECK_BLOCKLISTED:
+                console.log('companyId is retrieved:', companyId)
+                sendResponse({ isBlacklisted: true });
+                break;
+            default:
+                console.error('Unknown action');
+                break;
+        }
+    })();
+
+    // Prevent exit function on async
+    return true;
+});
+
