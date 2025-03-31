15738ac0e1e71bce593ecb75fa80be338eaac797
Alan Lee <pdh2ndtt@duck.com>
2025-03-31 14:42:56 +0900

    Implemented isBlackListedCompany

--- background.js
+++ background.js
@@ -29,3 +29,66 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
 });
 
+
+async function isBlackListedCompany(companyId) {
+
+    const result = await new Promise(resolve => {
+
+        const request = indexedDB.open(APP_DATABASE_NAME, 1);
+
+        request.onupgradeneeded = function (_event) {
+            const _db = _event.target.result;
+
+            const _isInitialized = _db.objectStoreNames.contains(
+                BLACKLIST_COMPANY_STORE_KEY
+            );
+            const _notInitialized = !_isInitialized;
+
+            if (_notInitialized) {
+                _db.createObjectStore(
+                    BLACKLIST_COMPANY_STORE_KEY,
+                    {
+                        keyPath: 'id',
+                        autoIncrement: true
+                    }
+                );
+            }
+
+            return _db;
+        }
+
+        request.onsuccess = async function (_event) {
+
+            const _db = _event.target.result;
+
+            const _transaction = _db.transaction(
+                BLACKLIST_COMPANY_STORE_KEY,
+                'readonly'
+            );
+
+            const _store = _transaction.objectStore(
+                BLACKLIST_COMPANY_STORE_KEY
+            );
+
+            const _getRequest = _store.get(companyId);
+
+            _getRequest.onsuccess = function () {
+                const __data = _getRequest.result;
+                const __result = !!__data;
+
+                resolve(__result);
+            };
+
+            _getRequest.onerror = function () {
+                console.log('Data Access Error');
+            };
+
+        }
+
+        request.onerror = function (_event) {
+            console.log('IndexedDB open is failed:', _event.target.error);
+        };
+    });
+
+    return result;
+}
